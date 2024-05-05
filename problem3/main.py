8f6aab026be2f47f00a3bdbf58b11b3e17ce69d3
Ahza Mazola <ahzamazola@gmail.com>
2024-05-05 21:28:24 +0700

    membuat fungsi prime_number

--- problem3/main.py
+++ problem3/main.py
@@ -1,5 +1,16 @@
 def prime_number(num):
-    return "error response'
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 if __name__ == '__main__':
     print(prime_number(11)) # "Prime"
