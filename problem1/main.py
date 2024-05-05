363d71b04f9adcc5513b5c2f3ac274eceefdf1d3
Ahza Mazola <ahzamazola@gmail.com>
2024-05-05 20:24:08 +0700

    menambah kode untuk nilai siswa

--- problem1/main.py
+++ problem1/main.py
@@ -2,6 +2,15 @@
 student_score = 80
 
 # Process: Your Solution Code Here
-
+if student_score <= 80:
+    print("Nilai A"),
+elif student_score <= 65 and student_score >= 79:
+    print ("Nilai B+"),
+elif student_score <= 50 and student_score >= 64:
+    print("Nilai B"),
+elif student_score <=35 and student_score >= 49:
+    print("Nilai C"),
+else:
+    print("Nilai D");
 
 # Output "Nilai A"
