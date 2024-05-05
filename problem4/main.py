510cabaede18fd621cf445323271217b624ceec7
Ahza Mazola <ahzamazola@gmail.com>
2024-05-05 22:31:35 +0700

    menambahkan function palindrome

--- problem4/main.py
+++ problem4/main.py
@@ -1,8 +1,8 @@
 def palindrome(input_string):
-     reverse_string = input_string [::-1]
-     if input_string == reverse_string:
+    reverse_string = input_string [::-1]
+    if input_string == reverse_string:
           return True
-     else:
+    else:
           return False
 
 if __name__ == '__main__':
